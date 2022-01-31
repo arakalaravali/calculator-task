0056b0156f07ccc927d5f9f3173379b68685897e
arakalaravali <96766976+arakalaravali@users.noreply.github.com>
2022-01-31 13:03:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,32 @@
 function add(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
-    var result=(num1)+(num2);
-    console.log(result);
-    document.getElementsByClassName("output")[0].innerHTML="ADDITION IS"+result;
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  var result=parseInt(num1)+parseInt(num2);
+  console.log(result);
+  document.getElementsByClassName("output")[0].innerHTML="ADDITION IS"+result;
 
 }
 function sub(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
-    var result=parseInt(num1)-parseInt(num2);
-    console.log(result);
-    document.getElementsByClassName("output")[0].innerText="subN IS"+result;
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  var result=parseInt(num1)-parseInt(num2);
+  console.log(result);
+  document.getElementsByClassName("output")[0].innerText="SUBTRACTION IS"+  result;
 
 }
 function mul(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
-    var result=parseInt(num1)*parseInt(num2);
-    console.log(result);
-    document.getElementsByClassName("output")[0].innerHTML="multON IS"+result;
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  var result=parseInt(num1)*parseInt(num2);
+  console.log(result);
+  document.getElementsByClassName("output")[0].innerHTML="MULTIPLICATION IS"+result;
 
 }
 function div(){
-    var num1=document.getElementById("first").Value;
-    var num2=document.getElementById("second").Value;
-    var result=parseInt(num1)/parseInt(num2);
-    console.log(result);
-    document.getElementsByClassName("output")[0].innerHTML="divisionN IS"+result;
+  var num1=document.getElementById("first").value;
+  var num2=document.getElementById("second").value;
+  var result=parseInt(num1)/parseInt(num2);
+  console.log(result);
+  document.getElementsByClassName("output")[0].innerHTML="DIVISION IS"+result;
 
 }
